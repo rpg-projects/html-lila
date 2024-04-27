52f787850729abc2325b2f5291fe2312ab5e239b
ElieneMontenegro <lila.amaral@hotmail.com>
2024-04-26 22:27:01 -0300

    fix: only one button to get html

--- main copy.js
+++ main copy.js
@@ -237,22 +237,7 @@ const getHTMLReady = () => {
 
   completeHtml = `${part1}${newText}${part2}`;
 
-  document.getElementById("postText").innerHTML = completeHtml;
+  //document.getElementById("postText").innerHTML = completeHtml;
+  navigator.clipboard.writeText(completeHtml);
   return completeHtml;
 };
-
-function copyToClipboard() {
-  // Get the text field
-  const copyText = getHTMLReady();
-  console.log("copyText :>> ", copyText);
-
-  //Select the text field
-  // copyText.select();
-  // copyText.setSelectionRange(0, 99999); // For mobile devices
-
-  // Copy the text inside the text field
-  navigator.clipboard.writeText(copyText);
-
-  // Alert the copied text
-  alert("Texto copiado");
-}
