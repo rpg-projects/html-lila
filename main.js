d3203f5e809b85025a1da0e048716acc8378e23b
Lila Montenegro <65774628+ElieneMontenegro@users.noreply.github.com>
2024-08-03 19:04:25 -0300

    mudar width do gif e margem

--- main.js
+++ main.js
@@ -26,8 +26,8 @@ const charlieHTML = `<center>
       <img
         src="https://64.media.tumblr.com/7a2f61cb6298a1dd71d54cbe03862035/67819a73e0192ed5-c9/s400x600/2151dd08f56c0e92e23ddf54356fc27e34f07050.gifv"
         style="
-          width: 95px;
-          margin-right: 5px;
+          width: 108px;
+          margin-right: 8px;
           padding: 3px;
           border: #f0a23a 3pt solid;
         "
@@ -71,10 +71,10 @@ const elodieHTML = `<center>
   >
     <div style="float: left">
       <img
-        src="https://64.media.tumblr.com/c3c604401ca3f7e843b5ddfec12c6170/1b20922c5c763baf-bb/s400x600/9c50dd4e0d18147c1b2092ecb3b21cbe23c4fd31.gifv"
+        src="https://64.media.tumblr.com/ca5d29841fbdb3460380d7e049b03c6e/885d52890a29ee4d-35/s250x400/4fb355b7de0a408ce96180e5aff8c893f11c281b.gifv"
         style="
-          width: 95px;
-          margin-right: 5px;
+          width: 108px;
+          margin-right: 8px;
           padding: 3px;
           border: #9ab973 3pt solid;
         "
@@ -118,8 +118,8 @@ const carcinhaHTML = `<center>
       <img
         src="https://64.media.tumblr.com/aaf721f9d3e71985223aacd3a13b4b0a/1b20922c5c763baf-c5/s400x600/208700e72630491f2fc0a77bab8f879c869f2265.gifv"
         style="
-          width: 95px;
-          margin-right: 5px;
+          width: 108px;
+          margin-right: 8px;
           padding: 3px;
           border: #872657 3pt solid;
         "
@@ -166,8 +166,8 @@ const jackieHTML = `<center>
       <img
         src="https://64.media.tumblr.com/ee3b4700d8cd0d178d0b4e70b740a394/1b20922c5c763baf-c9/s250x400/455b9c1a34447a8d10a6f515830339e72b23987a.gifv"
         style="
-          width: 95px;
-          margin-right: 5px;
+          width: 108px;
+          margin-right: 8px;
           padding: 3px;
           border: #d5732b 3pt solid;
         "
@@ -214,8 +214,8 @@ const oliviaHTML = `<center>
       <img
         src="//64.media.tumblr.com/717ff23123cec91083a8574e209b71cc/a004ff2e3f2c14cd-4e/s250x400/b4803e48d35a6843701f87d9c581d83c1e8d1284.gif"
         style="
-          width: 95px;
-          margin-right: 5px;
+          width: 108px;
+          margin-right: 8px;
           padding: 3px;
           border: #FF9966 3pt solid;
         "
@@ -260,10 +260,10 @@ const aspenHTML =  `<center>
   >
     <div style="float: left">
       <img
-        src="https://64.media.tumblr.com/9a4d7e1a1a938ca98c71cebbc1d3fb5e/db6c41c7b267291e-59/s500x750/016a81c3187e52dbd7665195a1543b7be3fc5bfe.gif"
+        src="https://64.media.tumblr.com/9a4d7e1a1a938ca98c71cebbc1d3fb5e/db6c41c7b267291e-59/s500x750/016a81c3187e52dbd76651108a1543b7be3fc5bfe.gif"
         style="
-          width: 95px;
-          margin-right: 5px;
+          width: 108px;
+          margin-right: 8px;
           padding: 3px;
           border: #779ECB 3pt solid;
         "
@@ -310,8 +310,8 @@ const hughieHTML = `<center>
       <img
         src="//64.media.tumblr.com/f408f7a1de6baa2a55bcfa5bae2f390b/a004ff2e3f2c14cd-26/s400x600/d3ed51def9332fce6ff9dbdae34d81f4bb56612b.gifv"
         style="
-          width: 95px;
-          margin-right: 5px;
+          width: 108px;
+          margin-right: 8px;
           padding: 3px;
           border: #7DA7DB 3pt solid;
         "
@@ -331,7 +331,7 @@ const hughieHTML = `<center>
 `;
 
 const kinsleyHTML = `
-<center><div style="width:650px;margin:14px;background-color:#99424a;color:#fff;font-family:'century gothic';font-size:10px;">  <p>   KINSLEY PALMER&nbsp; ─&nbsp; FILHA DE CIRCE&nbsp; ─&nbsp; PRIMEIRA CONTA&nbsp; ─&nbsp; 15 ANOS  </p>  <div style="background-color:#fff1f2;text-align:justify;border:#FF6E7B 3pt solid;padding:10px;line-height:14px;">    <div style="float:left;">      <img src="//64.media.tumblr.com/c36116a4b74ca1c0d74939ba9b334f0b/a004ff2e3f2c14cd-ba/s500x750/44fcedbed647cc0c547f6690b897019ec8892bf8.gif" style="width:95px;margin-right:5px;padding:3px;border:#FF6E7B 3pt solid;" alt="44fcedbed647cc0c547f6690b897019ec8892bf8.gif"></div>    <span style="font-family:tahoma;font-size:10pt;color:#000000;">  TEXTO  </span>  </div>  <p style="text-transform:uppercase;">Your head in the curtains, your heart like the fourth of July</p></div></center>`
+<center><div style="width:650px;margin:14px;background-color:#99424a;color:#fff;font-family:'century gothic';font-size:10px;">  <p>   KINSLEY PALMER&nbsp; ─&nbsp; FILHA DE CIRCE&nbsp; ─&nbsp; PRIMEIRA CONTA&nbsp; ─&nbsp; 15 ANOS  </p>  <div style="background-color:#fff1f2;text-align:justify;border:#FF6E7B 3pt solid;padding:10px;line-height:14px;">    <div style="float:left;">      <img src="//64.media.tumblr.com/c36116a4b74ca1c0d74939ba9b334f0b/a004ff2e3f2c14cd-ba/s500x750/44fcedbed647cc0c547f6690b897019ec8892bf8.gif" style="width:108px;margin-right:8px;padding:3px;border:#FF6E7B 3pt solid;" alt="44fcedbed647cc0c547f6690b897019ec8892bf8.gif"></div>    <span style="font-family:tahoma;font-size:10pt;color:#000000;">  TEXTO  </span>  </div>  <p style="text-transform:uppercase;">Your head in the curtains, your heart like the fourth of July</p></div></center>`
 
 const getCharHTML = (char) => {
 
