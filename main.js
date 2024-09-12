00ce0b91c0acde8293c28411a11ca0fba1890677
Lila Montenegro <65774628+ElieneMontenegro@users.noreply.github.com>
2024-09-12 10:33:43 -0300

    change ellie's gif

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ const charlieHTML = `<center>
       <img
         src="https://64.media.tumblr.com/7a2f61cb6298a1dd71d54cbe03862035/67819a73e0192ed5-c9/s400x600/2151dd08f56c0e92e23ddf54356fc27e34f07050.gifv"
         style="
-          width: 102px;
+          width: 103px;
           margin-right: 6px;
           padding: 3px;
           border: #f0a23a 3pt solid;
@@ -71,9 +71,9 @@ const elodieHTML = `<center>
   >
     <div style="float: left">
       <img
-        src="https://64.media.tumblr.com/ca5d29841fbdb3460380d7e049b03c6e/885d52890a29ee4d-35/s250x400/4fb355b7de0a408ce96180e5aff8c893f11c281b.gifv"
+        src="https://64.media.tumblr.com/8d16c1b3ba1910ef3e8ac980a4a7a015/3054562d6cb62d19-8f/s400x600/0b53a3bb5a32006b53fdd82c1435d8bcc264abcc.webp"
         style="
-          width: 102px;
+          width: 103px;
           margin-right: 6px;
           padding: 3px;
           border: #9ab973 3pt solid;
@@ -118,7 +118,7 @@ const carcinhaHTML = `<center>
       <img
         src="https://64.media.tumblr.com/aaf721f9d3e71985223aacd3a13b4b0a/1b20922c5c763baf-c5/s400x600/208700e72630491f2fc0a77bab8f879c869f2265.gifv"
         style="
-          width: 102px;
+          width: 103px;
           margin-right: 6px;
           padding: 3px;
           border: #872657 3pt solid;
@@ -166,7 +166,7 @@ const jackieHTML = `<center>
       <img
         src="https://64.media.tumblr.com/ee3b4700d8cd0d178d0b4e70b740a394/1b20922c5c763baf-c9/s250x400/455b9c1a34447a8d10a6f515830339e72b23987a.gifv"
         style="
-          width: 102px;
+          width: 103px;
           margin-right: 6px;
           padding: 3px;
           border: #d5732b 3pt solid;
@@ -214,7 +214,7 @@ const oliviaHTML = `<center>
       <img
         src="//64.media.tumblr.com/717ff23123cec91083a8574e209b71cc/a004ff2e3f2c14cd-4e/s250x400/b4803e48d35a6843701f87d9c581d83c1e8d1284.gif"
         style="
-          width: 102px;
+          width: 103px;
           margin-right: 6px;
           padding: 3px;
           border: #FF9966 3pt solid;
@@ -262,7 +262,7 @@ const aspenHTML =  `<center>
       <img
         src="//64.media.tumblr.com/9a4d7e1a1a938ca98c71cebbc1d3fb5e/db6c41c7b267291e-59/s500x750/016a81c3187e52dbd7665195a1543b7be3fc5bfe.gif"
         style="
-          width: 102px;
+          width: 103px;
           margin-right: 6px;
           padding: 3px;
           border: #779ECB 3pt solid;
@@ -310,7 +310,7 @@ const hughieHTML = `<center>
       <img
         src="https://i.pinimg.com/originals/eb/92/e8/eb92e89dada002f3ea4c3e13b9a32229.gif"
         style="
-          width: 102px;
+          width: 103px;
           margin-right: 6px;
           padding: 3px;
           border: #7DA7DB 3pt solid;
@@ -333,12 +333,12 @@ const hughieHTML = `<center>
 const kinsleyHTML = `
 <center><div style="width:650px;margin:14px;background-color:#99424a;color:#fff;font-family:'century gothic';font-size:10px;">  <p>   KINSLEY PALMER&nbsp; ─&nbsp; FILHA DE CIRCE&nbsp; ─&nbsp; PRIMEIRA CONTA&nbsp; ─&nbsp; 15 ANOS  </p>  
 <div style="background-color:#fff1f2;text-align:justify;border:#FF6E7B 3pt solid;padding:10px;line-height:14px;">    
-<div style="float:left;">      <img src="//64.media.tumblr.com/c36116a4b74ca1c0d74939ba9b334f0b/a004ff2e3f2c14cd-ba/s500x750/44fcedbed647cc0c547f6690b897019ec8892bf8.gif" style="width:102px;margin-right:6px;padding:3px;border:#FF6E7B 3pt solid;" alt="44fcedbed647cc0c547f6690b897019ec8892bf8.gif"></div>    
+<div style="float:left;">      <img src="//64.media.tumblr.com/c36116a4b74ca1c0d74939ba9b334f0b/a004ff2e3f2c14cd-ba/s500x750/44fcedbed647cc0c547f6690b897019ec8892bf8.gif" style="width:103px;margin-right:6px;padding:3px;border:#FF6E7B 3pt solid;" alt="44fcedbed647cc0c547f6690b897019ec8892bf8.gif"></div>    
 <span style="width:630px;font-family:tahoma;font-size:10pt;color:#000000;">  TEXTO  </span>  </div>  <p style="text-transform:uppercase;">Your head in the curtains, your heart like the fourth of July</p></div></center>`
 
 const matthewHTML = `<center><div style="width:650px; margin: 14px; background-color: #3b0707; color: #fff; font-family: 'century gothic'; font-size: 10px;">  
   <p> MATTHEW GRAYBACK&nbsp; ─&nbsp; FILHO DE HEFESTO&nbsp; ─&nbsp; PRIMEIRO VERÃO&nbsp; ─&nbsp;  16 ANOS </p>
-  <div style="background-color: #e1d5d5; text-align: justify; border: #4F0E0E 3pt solid; padding: 10px; line-height: 14px;"><div style="float: left"><img src="https://64.media.tumblr.com/e3325bbc531888c9a37e73bf80622e29/c562120c97effd1c-e4/s500x750/44ad647a39291ab28a6068c89d9535998d6a0030.webp" style="width: 102px; margin-right: 6px; padding: 3px; border: #4F0E0E 3pt solid;"></div>
+  <div style="background-color: #e1d5d5; text-align: justify; border: #4F0E0E 3pt solid; padding: 10px; line-height: 14px;"><div style="float: left"><img src="https://64.media.tumblr.com/e3325bbc531888c9a37e73bf80622e29/c562120c97effd1c-e4/s500x750/44ad647a39291ab28a6068c89d9535998d6a0030.webp" style="width: 103px; margin-right: 6px; padding: 3px; border: #4F0E0E 3pt solid;"></div>
     <span style="width: 630px;font-family: tahoma; font-size: 10pt; color: black">      TEXTO    </span>  </div>
   <p>ONCE UPON A DIFFERENT LIFE, WE RODE OUR BIKES INTO THE SKY</p>
 </div>
@@ -346,7 +346,7 @@ const matthewHTML = `<center><div style="width:650px; margin: 14px; background-c
 
 const skylarHTML = `
 <center><div style="    width: 650px;    margin: 14px;    background-color: #590e34;    color: #fff;    font-family: 'century gothic';    font-size: 10px;  ">  <p>    SKYLAR DILAURENTIS&nbsp; ─&nbsp; FILHO DE DIONÍSIO&nbsp; ─&nbsp; PRIMEIRO VERÃO&nbsp; ─&nbsp;  15 ANOS  </p>
-  <div style="      background-color: #e3cad6;      text-align: justify;      border: #872657 3pt solid;      padding: 10px;      line-height: 14px;    ">    <div style="float: left">      <img src="https://64.media.tumblr.com/fa0ed8e4c21d40503d36a2b6d6e776b2/3c937a31625d5dbf-60/s250x400/a625d8964e4fb40e798ef94af3f624c675f6b124.gifv" style="          width: 102px;          margin-right: 6px;          padding: 3px;          border: #872657 3pt solid;        ">    </div>
+  <div style="      background-color: #e3cad6;      text-align: justify;      border: #872657 3pt solid;      padding: 10px;      line-height: 14px;    ">    <div style="float: left">      <img src="https://64.media.tumblr.com/fa0ed8e4c21d40503d36a2b6d6e776b2/3c937a31625d5dbf-60/s250x400/a625d8964e4fb40e798ef94af3f624c675f6b124.gifv" style="          width: 103px;          margin-right: 6px;          padding: 3px;          border: #872657 3pt solid;        ">    </div>
     <span style="width: 630px;font-family: tahoma; font-size: 10pt; color: black">      TEXTO    </span>  </div>
   <p>HE TOOK THE MIDNIGHT TRAIN GOING ANYWHERE</p>
 </div>
@@ -355,7 +355,7 @@ const skylarHTML = `
 
 const milesHTML = `
 <center><div style="    width: 650px;    margin: 14px;    background-color: #7389ab;    color: #fff;    font-family: 'century gothic';    font-size: 10px;  ">  <p>    MILES CAPPELLI&nbsp; ─&nbsp; FILHO DE SELENE&nbsp; ─&nbsp; PRIMEIRO VERÃO&nbsp; ─&nbsp;  16 ANOS  </p>
-  <div style="      background-color: #ebeff6;      text-align: justify;      border: #A6B2C5 3pt solid;      padding: 10px;      line-height: 14px;    ">    <div style="float: left">      <img src="https://64.media.tumblr.com/e34efc3bea3a1b3a775563e0b639ce1a/3d539abcc8626995-d4/s250x400/a555ffbf3ba74d3f0201b168603ac202741f7af8.gifv" style="          width: 102px;          margin-right: 6px;          padding: 3px;          border: #A6B2C5 3pt solid;        ">    </div>
+  <div style="      background-color: #ebeff6;      text-align: justify;      border: #A6B2C5 3pt solid;      padding: 10px;      line-height: 14px;    ">    <div style="float: left">      <img src="https://64.media.tumblr.com/e34efc3bea3a1b3a775563e0b639ce1a/3d539abcc8626995-d4/s250x400/a555ffbf3ba74d3f0201b168603ac202741f7af8.gifv" style="          width: 103px;          margin-right: 6px;          padding: 3px;          border: #A6B2C5 3pt solid;        ">    </div>
     <span style="width: 630px;font-family: tahoma; font-size: 10pt; color: black">      TEXTO    </span>  </div>
   <p>YOUR FAITH WAS STRONG, BUT YOU NEEDED PROOF</p>
 </div>
